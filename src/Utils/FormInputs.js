7186ec5025cf15cef3d74f4e53d587c4edfc9a4a
manojkumarm1310 <132912460+manojkumarm1310@users.noreply.github.com>
2024-03-01 00:03:46 +0530

    change

--- src/Utils/FormInputs.js
+++ src/Utils/FormInputs.js
@@ -109,7 +109,7 @@ const FormInputs = ({
       setTaskName("");
       setTaskDescription("");
       setEmptyInputError(false);
-      navigate("/");
+      navigate("/ReactTodo/");
 
       setAddNotificationTitle(taskName);
       setAddNotification(true);
